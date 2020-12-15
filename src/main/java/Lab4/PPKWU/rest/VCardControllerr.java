62b27ebf0e11ff489ead6e454302d6375d93948d
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-12-15 15:15:50 +0100

    Fetched html document by quering in controller.

--- src/main/java/Lab4/PPKWU/rest/VCardControllerr.java
+++ src/main/java/Lab4/PPKWU/rest/VCardControllerr.java
@@ -4,7 +4,14 @@ import ezvcard.Ezvcard;
 import ezvcard.VCard;
 import ezvcard.VCardVersion;
 import ezvcard.property.StructuredName;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VCardControllerr {
 
+	private static final String URL_SERVICE = "https://panoramafirm.pl/szukaj?k=";
+
 	@GetMapping("/VCard")
-	public String countAllStatisticsEndpoint(
-		@RequestParam(value = "searchKey") String searchKey, HttpServletResponse response) {
+	public String generateVCardEndpoint(
+		@RequestParam(value = "searchKey") String searchKey, HttpServletResponse response)
+		throws IOException {
+		Document document = getDocumentFromUrl(URL_SERVICE + searchKey);
+		System.out.println(document);
 		return generateVCard();
 	}
 
@@ -32,4 +44,18 @@ public class VCardControllerr {
 		return Ezvcard.write(vcard).version(VCardVersion.V4_0).go();
 	}
 
+	private Document getDocumentFromUrl(String urlhtml) throws IOException {
+		URL url = new URL(urlhtml);
+		URLConnection connection = url.openConnection();
+		StringBuilder fromWebsite = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String line;
+
+		while ((line = reader.readLine()) != null) {
+			fromWebsite.append(line);
+		}
+		String htmlContent = fromWebsite.toString();
+		Document document = Jsoup.parse(htmlContent);
+		return document;
+	}
 }
