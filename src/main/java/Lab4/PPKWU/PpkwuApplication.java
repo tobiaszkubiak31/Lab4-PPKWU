64f855c0858ddf4734ef0dad06a325ed4ee1bf05
Tobiasz Kubiak <tobiaszkubiak31@gmail.com>
2020-12-15 14:29:23 +0100

    initialize spring project with rest dependency

---
+++ src/main/java/Lab4/PPKWU/PpkwuApplication.java
@@ -0,0 +1,13 @@
+package Lab4.PPKWU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lab4/PPKWU/PpkwuApplicationTests.java
@@ -0,0 +1,13 @@
+package Lab4.PPKWU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkwuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
